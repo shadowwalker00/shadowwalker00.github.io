9a0cdd733d67fdfb52887351a212f4eb1e0dd578
guanghao0419@outlook.com <1936381013@qq.com>
2020-04-10 17:17:54 -0700

    Site updated: 2020-04-10 17:17:54

--- photos/ins.js
+++ photos/ins.js
@@ -117,8 +117,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://raw.githubusercontent.com/shadowwalker00/album/master/min_photos/' + data.link[i];
-            var src = 'https://raw.githubusercontent.com/shadowwalker00/album/master/photos/' + data.link[i];
+            var minSrc = 'https://raw.githubusercontent.com/shadowwalker00/Blog_Album/master/min_photos/' + data.link[i];
+            var src = 'https://raw.githubusercontent.com/shadowwalker00/Blog_Album/master/photos/' + data.link[i];
             
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
